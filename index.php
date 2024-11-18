e4dd5dece9e7852456e27a02cd8550890550af72
cookepoli <a.cookepoli@gmail.com>
2024-11-18 14:45:44 -0500

    altered forms based on open lab

--- index.php
+++ index.php
@@ -36,11 +36,8 @@ if ($option != null) {
 
         case UPDATE:
             if ((0 == $_POST['new-attribute']) && ("" == $_POST['pattern'])) {
-                echo '<div id="error">One or both fields were empty, ' .
-                    'but both must be filled out. Please try again.</div>' . "\n";
-            } if ((update($_POST['current-attribute'], $_POST['new-attribute'],
-                    $_POST['query-attribute'], $_POST['pattern']) == -1)){
-                echo '<div id="error">Duplicate primary key.</div>' . "\n";
+                echo '<div id="error">One or more fields were empty, ' .
+                    'but all must be filled out. Please try again.</div>' . "\n";
             } else {
                 update($_POST['current-attribute'], $_POST['new-attribute'],
                     $_POST['query-attribute'], $_POST['pattern']);
@@ -56,9 +53,9 @@ if ($option != null) {
                 echo '<div id="error">At least one field in your insert request ' .
                      'is empty. Please try again.</div>' . "\n";
             } else {
-                insert($_POST['website-name'],$_POST['website-url'],$_POST['first-name'],
-                        $_POST['last-name'],$_POST['email'],$_POST['username'],
-                        $_POST['password'],$_POST['comment']);
+                insert($_POST['website-name'], $_POST['website-url'], $_POST['first-name'],
+                        $_POST['last-name'], $_POST['email'], $_POST['username'],
+                        $_POST['password'], $_POST['comment']);
             }
             break;
 
@@ -66,11 +63,11 @@ if ($option != null) {
             if (("" == $_POST['current-attribute']) || ("" == $_POST['pattern'])) {
             echo '<div id="error">At least one field in your delete procedure ' .
                  'is empty. Please try again.</div>' . "\n";
-        } else {
+            } else {
             delete($_POST['current-attribute'], $_POST['pattern']);
-        }
+            }
 
-        break;
+            break;
 
     }
 }
@@ -81,20 +78,20 @@ if ($option != null) {
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
   <fieldset>
     <legend>Search</legend>
-    SELECT FROM * WHERE
+    SELECT * FROM all_relations WHERE
     <select name="attribute" id="attribute">
-        <option>website.website_id</option>
-        <option>user.user_id</option>
-        <option>website_name</option>
-        <option>website_url</option>
-        <option>first_name</option>
-        <option>last_name</option>
-        <option>email</option>
-        <option>username</option>
-        <option>password</option>
-        <option>creation_time</option>
-        <option>comment</option>
-        <option>all</option>
+        <option value = "website.website_id">Website ID</option>
+        <option value = "user.user_id">User ID</option>
+        <option value = "website_name">Website Name</option>
+        <option value = "website_url">Website URL</option>
+        <option value = "first_name">First Name</option>
+        <option value = "last_name">Last Name</option>
+        <option value = "email">Email Address</option>
+        <option value = "username">Username</option>
+        <option value = "password">Password</option>
+        <option value = "creation_time">Time of Creation</option>
+        <option value = "option">Comment</option>
+        <option value = "all">Full Database</option>
     </select>
     LIKE
     <input type="text" name="search" autofocus required>
@@ -108,33 +105,33 @@ if ($option != null) {
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
   <fieldset>
     <legend>Update</legend>
-    UPDATE relation SET
+    UPDATE all_relations SET
     <select name="current-attribute" id="current-attribute">
-        <option>website.website_id</option>
-        <option>user.user_id</option>
-        <option>website_name</option>
-        <option>website_url</option>
-        <option>first_name</option>
-        <option>last_name</option>
-        <option>email</option>
-        <option>username</option>
-        <option>password</option>
-        <option>creation_time</option>
-        <option>comment</option>
+        <option value = "website_name">Website Name</option>
+        <option value = "website_url">Website URL</option>
+        <option value = "first_name">First Name</option>
+        <option value = "last_name">Last Name</option>
+        <option value = "email">Email Address</option>
+        <option value = "username">Username</option>
+        <option value = "password">Password</option>
+        <option value = "creation_time">Time of Creation</option>
+        <option value = "option">Comment</option>
+        <option value = "all">Full Database</option>
     </select>
     = <input type="text" name="new-attribute" required> WHERE
     <select name="query-attribute" id="query-attribute">
-        <option>website.website_id</option>
-        <option>user.user_id</option>
-        <option>website_name</option>
-        <option>website_url</option>
-        <option>first_name</option>
-        <option>last_name</option>
-        <option>email</option>
-        <option>username</option>
-        <option>password</option>
-        <option>creation_time</option>
-        <option>comment</option>
+        <option value = "website.website_id">Website ID</option>
+        <option value = "user.user_id">User ID</option>
+        <option value = "website_name">Website Name</option>
+        <option value = "website_url">Website URL</option>
+        <option value = "first_name">First Name</option>
+        <option value = "last_name">Last Name</option>
+        <option value = "email">Email Address</option>
+        <option value = "username">Username</option>
+        <option value = "password">Password</option>
+        <option value = "creation_time">Time of Creation</option>
+        <option value = "option">Comment</option>
+        <option value = "all">Full Database</option>
     </select>
     = <input type="text" name="pattern" required>;
     <input type="hidden" name="submitted" value="2">
@@ -145,15 +142,17 @@ if ($option != null) {
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
   <fieldset>
     <legend>Insert</legend>
-    INSERT INTO relation VALUES <br> <br>
-    ( <input type="text" name="website-name" placeholder="website_name" required>,
-                                    <input type="text" name="website-url" placeholder="website_url" required>,
-                                    <input type="text" name="first-name" placeholder="first_name" required>,
-                                    <input type="text" name="last-name" placeholder="last_name" required>,
-                                    <input type="text" name="email" placeholder="email" required>,
-                                    <input type="text" name="username" placeholder="username" required>,
-                                    <input type="text" name="password" placeholder="password" required>,
-                                    <textarea id="comment" name="comment" placeholder="comment" required></textarea>);
+    INSERT INTO all_relations VALUES <br> <br>
+    (
+        <input type="text" name="website-name" placeholder="Website Name" required>,
+        <input type="text" name="website-url" placeholder="Website URL" required>,
+        <input type="text" name="first-name" placeholder="First Name" required>,
+        <input type="text" name="last-name" placeholder="Last Name" required>,
+        <input type="text" name="email" placeholder="Email Address" required>,
+        <input type="text" name="username" placeholder="Username" required>,
+        <input type="text" name="password" placeholder="Password" required>,
+        <textarea id="comment" name="comment" placeholder="Comment" required></textarea>
+    );
     <input type="hidden" name="submitted" value="3">
     <p><input type="submit" value="insert"></p>
   </fieldset>
@@ -162,19 +161,20 @@ if ($option != null) {
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
   <fieldset>
     <legend>Delete</legend>
-    DELETE FROM relation WHERE
+    DELETE FROM all_relations WHERE
     <select name="current-attribute" id="current-attribute">
-        <option>website.website_id</option>
-        <option>user.user_id</option>
-        <option>website_name</option>
-        <option>website_url</option>
-        <option>first_name</option>
-        <option>last_name</option>
-        <option>email</option>
-        <option>username</option>
-        <option>password</option>
-        <option>creation_time</option>
-        <option>comment</option>
+        <option value = "website.website_id">Website ID</option>
+        <option value = "user.user_id">User ID</option>
+        <option value = "website_name">Website Name</option>
+        <option value = "website_url">Website URL</option>
+        <option value = "first_name">First Name</option>
+        <option value = "last_name">Last Name</option>
+        <option value = "email">Email Address</option>
+        <option value = "username">Username</option>
+        <option value = "password">Password</option>
+        <option value = "creation_time">Time of Creation</option>
+        <option value = "option">Comment</option>
+        <option value = "all">Full Database</option>
     </select>
     = <input type="text" name="pattern" required>;
       <input type="hidden" name="submitted" value="4">
